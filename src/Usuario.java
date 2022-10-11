d57fdd00781171317138f71ca491d6f809a002a7
Tunin33048 <antonio.neves.toledo@gmail.com>
2022-10-11 15:16:10 -0300

    Código completo

--- src/Usuario.java
+++ src/Usuario.java
@@ -2,13 +2,16 @@ public class Usuario {
 
     private String nome;
     private String email;
+    Instagram instagram = new Instagram();
 
     Usuario(RedeSocial[] redeSociais) {
         redeSociais[0].postarComentario();
         redeSociais[0].postarFoto();
         redeSociais[0].postarVideo();
-        System.out.println("-----------------------------");
+        System.out.println("--------------------------------------\n");
         redeSociais[1].curtirPublicacao();
+        instagram.fazStreaming();
+        instagram.compartilhar();
     }
 
     Usuario(String nome, String email) {
